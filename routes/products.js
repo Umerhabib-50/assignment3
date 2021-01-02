48ff347e68d1af21e941a039586a64df0e4c8992
Umerhabib-50 <71447236+Umerhabib-50@users.noreply.github.com>
2021-01-02 15:01:01 +0500

    3

--- routes/products.js
+++ routes/products.js
@@ -16,19 +16,4 @@ router.post("/add", async function (req, res, next) {
   await product.save();
   res.redirect("/products");
 });
-router.get("/delete/:id", async function (req, res, next) {
-  let product = await Product.findByIdAndDelete(req.params.id);
-  res.redirect("/products");
-});
-router.get("/edit/:id", async function (req, res, next) {
-  let product = await Product.findById(req.params.id);
-  res.render("products/edit", { product });
-});
-router.post("/edit/:id", async function (req, res, next) {
-  let product = await Product.findById(req.params.id);
-  product.name = req.body.name;
-  product.price = req.body.price;
-  await product.save();
-  res.redirect("/products");
-});
 module.exports = router;
